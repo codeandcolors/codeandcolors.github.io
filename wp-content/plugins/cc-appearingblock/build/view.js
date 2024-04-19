0b60c5e4343aa79c417a529f2ab55d28651d27ee
Michael Langbein <michael_langbein@hotmail.de>
2024-04-19 22:19:40 +0200

    new page

--- wp-content/plugins/cc-appearingblock/build/view.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";const e=document.getElementsByClassName("animatedBlock"),t=new IntersectionObserver(((e,t)=>{for(const t of e)t.isIntersecting&&t.target.classList.add("viewed")}),{rootMargin:"-20%"});for(const s of e)t.observe(s)})();
